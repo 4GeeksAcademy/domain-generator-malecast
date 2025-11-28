2554f7ec5401b88e8475479ba7f525f5dbbec71d
malecast <malecast.12@gmail.com>
2025-11-28 05:39:29 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-import "bootstrap";
-import "./style.css";
+let pronouns = ["the", "your", "my"];
+let adjectives = ["great", "dark", "wonderful"];
+let nouns = ["wizard", "dragon", "pages", "internet", "studio"];
+let domainExtensions = [".com", ".net", ".io", ".es"];
 
+let domainNames = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+        for (const noun of nouns) {
+            for (const ext of domainExtensions) {
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+                let domain = "";
+
+                
+                if (noun === "pages" && ext === ".es") {
+                    let short = noun.slice(0, -2); 
+                    domain = pronoun + adjective + short + ext;         
+                }
+                else if (noun === "studio" && ext === ".io") {
+                    let short = noun.slice(0, -2); 
+                    domain = pronoun + adjective + short + ext;         
+                }
+                else if (noun === "internet" && ext === ".net") {
+                    let short = noun.slice(0, -3); 
+                    domain = pronoun + adjective + short + ext;          
+                }
+                
+                else {
+                    domain = pronoun + adjective + noun + ext;
+                }
+
+                
+                if (domain !== "") {
+                    domainNames.push(domain);
+                }
+
+            }
+        }
+    }
+}
+
+console.log("Generated Domain Names:\n" + domainNames.join("\n"));
+
